36b5f0ae32b13f37684745a395ddc005a72e4fb7
coreyarch1234 <harrilal@mit.edu>
2017-09-15 22:25:36 -0700

    got rid of note controller

---
+++ rails_scope/app/controllers/concerns/notes_helper.rb
@@ -0,0 +1,6 @@
+module NotesHelper
+    def json_creator()
+        json = {"title": "Manu Prakash Goes to Vegas", "author": "Manu Prakash"}
+        return json
+    end
+end
--- rails_scope/app/controllers/note_controller.rb
+++
@@ -1,9 +0,0 @@
-class NoteController < ApplicationController
-  def index
-      @test_json = {"title": "Manu Prakash Goes to Vegas", "author": "Manu Prakash"}
-      render :json => @test_json
-  end
-
-  def create
-  end
-end
